8783d1ee9b860327bf07727a7f3c6ae5b3594ac0
dotan <dotcompanyofficial@gmail.com>
2026-02-02 00:32:53 +0200

    add constant veraibales for ROLES  and ORDER STATUS

---
+++ src/config/constants.py
@@ -0,0 +1,12 @@
+ROLES = {
+    'ADMIN': 'admin',
+    'MANAGER': 'manager',
+    'CUSTOMER': 'customer'
+}
+
+ORDER_STATUS = {
+    'PENDING': 'pending',
+    'PAID': 'paid',
+    'SHIPPED': 'shipped',
+    'CANCELLED': 'cancelled'
+}
